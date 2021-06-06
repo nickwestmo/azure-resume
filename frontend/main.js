e4ae3273ec85e4de265ab7b3b141520df93fda9f
Nick Westmoreland <nickwestmo@gmail.com>
2021-06-05 23:03:47 -0500

    Updated index.html and created main.js file for my visit count

---
+++ frontend/main.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVistCount();
+})
+
+const functionApi = '';
+
+const getVistCount = () => {
+    let count = 30;
+    fetch(functionApi).then(Response => {
+        return Response.json()
+
+    }).then(response =>{
+        console.log("Website called function API.")
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+
+    });
+    return count;
+}
